fc4db1026b76e73ea333e064828f7a6d8fd60d33
Jose Blandon <jose.blandon@ceiba.com.co>
2018-05-22 14:12:27 -0500

    Se creo la estructura del proyecto y la vista minima del main activity

--- app/src/main/java/co/com/ceiba/parqueadero/view/cobrar/ActivityCobrar.java
+++ app/src/main/java/co/com/ceiba/parqueadero/view/cobrar/ActivityCobrar.java
@@ -1,13 +1,15 @@
-package co.com.ceiba.parqueadero;
+package co.com.ceiba.parqueadero.view.cobrar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import co.com.ceiba.parqueadero.R;
+
+public class ActivityCobrar extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_cobrar);
     }
 }
---
+++ app/src/main/java/co/com/ceiba/parqueadero/view/ingresar/ActivityIngresar.java
@@ -0,0 +1,15 @@
+package co.com.ceiba.parqueadero.view.ingresar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import co.com.ceiba.parqueadero.R;
+
+public class ActivityIngresar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ingresar);
+    }
+}
