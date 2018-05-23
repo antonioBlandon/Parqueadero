5c07d4aa410eee1b17ab080e4710810fb1b6758a
Jose Blandon <jose.blandon@ceiba.com.co>
2018-05-23 09:55:54 -0500

    Se agregaron las pruebas para validar cantidad de carros

--- app/src/main/java/co/com/ceiba/parqueadero/model/Carro.java
+++ app/src/main/java/co/com/ceiba/parqueadero/model/Carro.java
@@ -1,7 +1,9 @@
 package co.com.ceiba.parqueadero.model;
 
 public class Carro extends Vehiculo {
-    public Carro(String placa, String fechaIngreso, String fechaSalida, int tiempoEnParqueadero, double valorApagarParqueadero) {
+
+    public Carro(String placa, long fechaIngreso, long fechaSalida, int tiempoEnParqueadero, double valorApagarParqueadero) {
         super(placa, fechaIngreso, fechaSalida, tiempoEnParqueadero, valorApagarParqueadero);
     }
+
 }
---
+++ app/src/test/java/co/com/ceiba/parqueadero/model/VigilanteImplTest.java
@@ -0,0 +1,68 @@
+package co.com.ceiba.parqueadero.model;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class VigilanteImplTest {
+
+    private Vigilante vigilante;
+
+    @Before
+    public void preparaData(){
+        vigilante = new VigilanteImpl();
+    }
+
+    @Test
+    public void testValidarCantidadCarros(){
+        //Arrange
+        int cantidadCarrosActual = 20;
+        //Act
+        boolean puedeIngresar = vigilante.validarCantidadCarros(cantidadCarrosActual);
+        //Assert
+        Assert.assertEquals(true,puedeIngresar);
+    }
+
+    @Test
+    public void testValidarCantidadCarros1(){
+        //Arrange
+        int cantidadCarrosActual = 21;
+        //Act
+        boolean puedeIngresar = vigilante.validarCantidadCarros(cantidadCarrosActual);
+        //Assert
+        Assert.assertEquals(false,puedeIngresar);
+    }
+
+    @Test
+    public void testValidarCantidadMotos(){
+
+    }
+
+    @Test
+    public void testValidarPlaca(){
+
+    }
+
+    @Test
+    public void testCalcularTiempoVehiculoParqueadero(){
+
+    }
+
+    @Test
+    public void testCobrarParqueadero(){
+
+    }
+
+    @Test
+    public void testSacarVehiculo(){
+
+    }
+
+    @Test
+    public void testObtenerCantidadDeDiasYHoras(){
+
+    }
+
+}
